8a0710d2ceb23c0ca4faf6b2a342e1b214c3f063
Kapitonov Roman <kapitroman@yandex.ru>
2020-10-28 10:15:34 +0300

    блок Хочу поехать

--- source/js/main.js
+++ source/js/main.js
@@ -13,10 +13,10 @@
   var errorMessageTel = sectionOrderCall.querySelectorAll('.order-call span')[1];
 
   var isStorageSupport = true;
-  var storageName = "";
+  var storageTel = "";
 
   try {
-    storageName = localStorage.getItem('name');
+    storageTel = localStorage.getItem('tel');
   } catch (err) {
     isStorageSupport = false;
   }
@@ -51,7 +51,7 @@
     evt.preventDefault();
     sectionOrderCall.classList.add('modal-show');
     body.classList.add('body-lock');
-    if (storageName) {
+    if (storageTel) {
       inputName.value = localStorage.getItem('name');
       inputTel.value = localStorage.getItem('tel');
     }
@@ -69,10 +69,10 @@
     }
     inputName.focus();
   });
-
+  /*
   errorMessageName.classList.add('error-hide');
   errorMessageTel.classList.add('error-hide');
-
+  */
   sectionOrderCall.addEventListener('click', modalCloseClickHandler);
   sectionOrderDelivered.addEventListener('click', modalCloseClickHandler);
 
@@ -176,5 +176,105 @@
   }
 
   programTabsList.addEventListener('click', tabsToggleClickHandler);
+
+})();
+
+(function () {
+  //var linkOrderCall = document.querySelector('.page-header__order-call');
+  //var sectionOrderCall = document.querySelectorAll('.order-call')[0];
+  var sectionOrderDelivered = document.querySelectorAll('.order-call')[1];
+  var form = document.querySelector('.desire-trip__form');
+  var body = document.querySelector('body');
+  //var inputName = sectionOrderCall.querySelector('[type="text"]');
+  var inputTel = form.querySelector('[type="tel"]');
+  //var errorMessageName = sectionOrderCall.querySelectorAll('.order-call span')[0];
+  var errorMessageTel = form.querySelector('span');
+
+  var isStorageSupport = true;
+  var storageTel = "";
+
+  try {
+    storageTel = localStorage.getItem('tel');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  function closeModal() {
+
+    if (sectionOrderDelivered.classList.contains('modal-show')) {
+      sectionOrderDelivered.classList.remove('modal-show');
+    }
+    body.classList.remove('body-lock');
+  }
+
+  function modalCloseClickHandler(evt) {
+    if (!evt.target.closest('form')) {
+      evt.preventDefault();
+      return closeModal();
+    }
+    if (evt.target.closest('.order-call__close')) {
+      evt.preventDefault();
+      return closeModal();
+    }
+    if (evt.target.closest('.order-call__form--delivered [type="submit"]')) {
+      evt.preventDefault();
+      return closeModal();
+    }
+    return;
+  }
+
+  if (storageTel) {
+    inputTel.value = localStorage.getItem('tel');
+  }
+  if (!inputTel.value) {
+    inputTel.classList.add('input-default');
+  }
+  if (inputTel.value) {
+    inputTel.classList.add('input-valid');
+  }
+
+  sectionOrderDelivered.addEventListener('click', modalCloseClickHandler);
+
+  inputTel.addEventListener('blur', function() {
+    if (inputTel.checkValidity()) {
+      inputTel.classList.add('input-valid');
+    }
+    if (inputTel.value && !inputTel.checkValidity()) {
+      inputTel.classList.add('input-invalid');
+      errorMessageTel.classList.add('error-show');
+    }
+    return;
+  })
+
+  inputTel.addEventListener('focus', function() {
+    if (inputTel.classList.contains('input-valid')) {
+      inputTel.classList.remove('input-valid');
+    }
+    if (inputTel.classList.contains('input-invalid')) {
+      inputTel.classList.remove('input-invalid');
+      errorMessageTel.classList.remove('error-show');
+    }
+    return;
+  })
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    if (isStorageSupport) {
+      localStorage.setItem('tel', inputTel.value);
+    }
+    closeModal();
+    sectionOrderDelivered.classList.add('modal-show');
+    body.classList.add('body-lock');
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+
+      if (sectionOrderDelivered.classList.contains("modal-show")) {
+        closeModal();
+      }
+    }
+  });
 
 })();
